2411098b8010682613f8b0f3c928da40c1591962
Sudhanshu Sam <thomsoiitr15@gmail.com>
2024-11-29 17:09:13 +0530

    added all category imports

--- scraper_kids_all.py
+++ scraper_kids_all.py
@@ -2,7 +2,7 @@ from imports_common import *
 
 if __name__ == "__main__":
     # Step 1: Scrape main category and subcategories
-    scrape_hnm_category("men", [5, 6, 7, 8])
+    scrape_hnm_category("kids", [4, 5, 6, 7])
 
     # Step 2: Scrape products in parallel
-    scrape_hnm_categories_parallel("men")
+    scrape_hnm_categories_parallel("kids")
